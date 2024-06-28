fb026c1fdbc913491e3f4558142d73c816611d0a
dario.colzi <d.colzi@planetweb.it>
2024-06-28 12:54:53 +0200

    Added navigation buttons to detail page

--- src/app/details/detail.component.ts
+++ src/app/details/detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, inject } from "@angular/core";
+import { ChangeDetectorRef, Component, OnInit, inject } from "@angular/core";
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from "@angular/material/icon";
 import { MatTooltipModule } from "@angular/material/tooltip";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 import { IRiga } from "../interfaces/riga.interface";
 import { ExcellService } from "../excell.service";
@@ -15,6 +16,7 @@ import { CommonModule } from "@angular/common";
     standalone: true,
     imports: [
         CommonModule,
+        MatButtonModule,
         MatCardModule,
         MatDividerModule,
         MatIconModule,
@@ -24,47 +26,114 @@ import { CommonModule } from "@angular/common";
     styleUrl: './detail.component.scss'
 })
 export class DetailComponent implements OnInit {
-    readonly id: number = -1;
+    private id: number = -1;
+    private detailTypeDisc: number = -1;
 
-    iconaTabella: string;
-    tooltipDex: string;
+    protected firstDetailId: number;
+    protected previousDetailId: number;
+    protected nextDetailId: number;
+    protected lastDetailId: number;
+
+    protected disableFirst: boolean = true;
+    protected disablePrevious: boolean = true;
+    protected disableNext: boolean = true;
+    protected disableLast: boolean = true;
+
+    iconaTabella: string = "error";
+    tooltipDex: string = "Errore";
 
     route: ActivatedRoute = inject(ActivatedRoute);
     dataSource!: IRiga;
 
-    constructor(private excellService: ExcellService) {
+    constructor(
+        private cdr: ChangeDetectorRef,
+        private excellService: ExcellService,
+        private router: Router
+    ) {
         ToolbarComponent.staticTabIndex = -1;
         
         this.id = Number(this.route.snapshot.params['id']);
 
-        this.iconaTabella = "error";
-        this.tooltipDex = "Errore";
+        this.firstDetailId = this.id;
+        this.previousDetailId = this.id;
+        this.nextDetailId = this.id;
+        this.lastDetailId = this.id;
 
         if (this.id < 0)
             console.log("[Detail constructor] Detail ID: " + this.id);
     }
 
     ngOnInit(): void {
+        // Subscribe to route parameter changes
+        this.route.params.subscribe(params => {
+            let paramId: number = +params['id'];
+
+            if (this.id != paramId) {
+                this.id = paramId;
+                
+                window.location.reload();
+                //this.router.navigate(['/detail', this.id]);
+            }
+        });
+
         this.dataSource = this.excellService.getDetail(this.id);
         
+        let disc: number = -1;
+
         switch(this.dataSource.discriminante) {
             case "G":
                 this.iconaTabella = "groups_3";
                 this.tooltipDex = "Gruppi multidisciplinari";
+                disc = ExcellService.DISC_GMTABLE;
                 break;
 
             case "A":
                 this.iconaTabella = "local_hospital";
                 this.tooltipDex = "Ambulatorio";
+                disc = ExcellService.DISC_AMBULATORIO;
                 break;
 
             case "?":
                 this.iconaTabella = "live_help";
                 this.tooltipDex = "Da decidere";
+                disc = ExcellService.DISC_DADECIDERE;
                 break;
 
             default:
                 break;
         }
+
+        if (disc != -1) {
+            this.detailTypeDisc = disc;
+
+            let navigationIds: number[] = [4];
+
+            navigationIds = this.excellService.getNavigationIds(this.id, this.detailTypeDisc);
+
+            this.firstDetailId = navigationIds[0];
+            this.previousDetailId = navigationIds[1];
+            this.nextDetailId = navigationIds[2];
+            this.lastDetailId = navigationIds[3];
+
+            this.disableFirst = this.firstDetailId == this.id;
+            this.disablePrevious = this.previousDetailId == this.id;
+            this.disableNext = this.nextDetailId == this.id;
+            this.disableLast = this.lastDetailId == this.id;
+        }
+    }
+
+    navigateFirstDetail(): void {
+        this.router.navigate(['/detail/', this.firstDetailId]);
+    }
+    navigatePreviousDetail(): void {
+        this.router.navigate(['/detail/', this.previousDetailId]);
+    }
+
+    navigateNextDetail(): void {
+        this.router.navigate(['/detail/', this.nextDetailId]);
+    }
+
+    navigateLastDetail(): void {
+        this.router.navigate(['/detail/', this.lastDetailId]);
     }
 }
--- src/app/excell.service.ts
+++ src/app/excell.service.ts
@@ -22,6 +22,7 @@ export class ExcellService {
 
   constructor() { }
 
+  // PUBLIC METHODS
   public getTable(id: number): any {
     switch (id) {
       case ExcellService.DISC_GMTABLE:
@@ -113,6 +114,91 @@ export class ExcellService {
     }
   }
 
+  public getNavigationIds(id: number, disc: number): number[] {
+    /*
+      [0] => first
+      [1] => previous
+      [2] => next
+      [3] => last
+    */
+    let resIds: number[] = [4];
+
+    switch (disc) {
+      case ExcellService.DISC_AMBULATORIO: // Ambulatorio
+      {
+        let ambulatorioTableData: IAmbulatorioTableRow[] = this.getAmbulatorioTableDataFromLocalStorage();
+
+        let currentIndex: number = ambulatorioTableData.findIndex((row: IAmbulatorioTableRow, index: number) => {
+          if (row.row_number == id)
+            return true;
+          else
+            return false;
+        });
+        
+        if (currentIndex != -1) {
+          resIds[0] = ambulatorioTableData[0].row_number;
+          resIds[1] = currentIndex == 0 ? id : ambulatorioTableData[currentIndex - 1].row_number;
+          resIds[2] = currentIndex == ambulatorioTableData.length - 1 ? id : ambulatorioTableData[currentIndex + 1].row_number;
+          resIds[3] = ambulatorioTableData[ambulatorioTableData.length - 1].row_number;
+        } else 
+          resIds = [id, id, id, id];
+
+        break;
+      }
+
+      case ExcellService.DISC_DADECIDERE: // Da decidere
+      {
+        let daDecidereTableData: IDaDecidereTableRow[] = this.getDaDecidereTableDataFromLocalStorage();
+
+        let currentIndex: number = daDecidereTableData.findIndex((row: IDaDecidereTableRow, index: number) => {
+          if (row.row_number == id)
+            return true;
+          else
+            return false;
+        });
+        
+        if (currentIndex != -1) {
+          resIds[0] = daDecidereTableData[0].row_number;
+          resIds[1] = currentIndex == 0 ? id : daDecidereTableData[currentIndex - 1].row_number;
+          resIds[2] = currentIndex == daDecidereTableData.length - 1 ? id : daDecidereTableData[currentIndex + 1].row_number;
+          resIds[3] = daDecidereTableData[daDecidereTableData.length - 1].row_number;
+        } else 
+          resIds = [id, id, id, id];
+
+        break;
+      }
+
+      case ExcellService.DISC_GMTABLE: // Gruppi multidisciplinari
+      {
+        let gmTableData: IGmTableRow[] = this.getGmTableDataFromLocalStorage();
+
+        let currentIndex: number = gmTableData.findIndex((row: IDaDecidereTableRow, index: number) => {
+          if (row.row_number == id)
+            return true;
+          else
+            return false;
+        });
+        
+        if (currentIndex != -1) {
+          resIds[0] = gmTableData[0].row_number;
+          resIds[1] = currentIndex == 0 ? id : gmTableData[currentIndex - 1].row_number;
+          resIds[2] = currentIndex == gmTableData.length - 1 ? id : gmTableData[currentIndex + 1].row_number;
+          resIds[3] = gmTableData[gmTableData.length - 1].row_number;
+        } else 
+          resIds = [id, id, id, id];
+
+        break;
+      }
+
+      default:
+        resIds = [id, id, id, id]
+        return resIds;
+    }
+
+    return resIds;
+  }
+
+  // PRIVATE METHODS
   private getAmbulatorioTableDataFromLocalStorage(): IAmbulatorioTableRow[] {
     if (localStorage.getItem(ExcellService.LOCALSTORAGE_ITEM_NAME_AMBULATORIO_TABLE_DATA) != null) {
       let gmTableData: IAmbulatorioTableRow[] = JSON.parse(localStorage.getItem(ExcellService.LOCALSTORAGE_ITEM_NAME_AMBULATORIO_TABLE_DATA)!);
